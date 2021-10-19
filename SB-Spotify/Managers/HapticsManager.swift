1ccc8517d02b6447d4bb787ff91e11ccb706fe41
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-19 09:15:36 +0700

    Create some manager files

---
+++ SB-Spotify/Managers/HapticsManager.swift
@@ -0,0 +1,12 @@
+//
+//  HapticsManager.swift
+//  SB-Spotify
+//
+//  Created by Danh Tu on 19/10/2021.
+//
+
+import Foundation
+
+// Haptic technology
+// AKA Kinaesthetic communication or 3D touch
+// Refering to any technology that can create an exprerience of touch by applying forces, vibrations, or motions to the user.
