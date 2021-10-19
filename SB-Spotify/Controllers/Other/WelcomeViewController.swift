317266da4715e2c8bbbe735a1340800e388db986
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-19 09:10:42 +0700

    Create Other Controller folder

---
+++ SB-Spotify/Controllers/Other/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  SB-Spotify
+//
+//  Created by Danh Tu on 19/10/2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
